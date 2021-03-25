94905284c213e5d18084e3a16a2bd5e36c88ca6b
Francesco Paciello <kekkopacio@gmail.com>
2021-03-25 14:50:10 +0100

    Initial Commit

---
+++ WhatsApp/WhatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppApp.swift
+//  WhatsApp
+//
+//  Created by Kekko Paciello on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
